fix: normalization

Assigment1.py
@@ -2,6 +2,8 @@ from load_data import load_batch
 from classifier import Classifier
 import numpy as np
 
+
+
 def load_data():
     train_data = load_batch("cifar-10-batches-py/data_batch_1")
     validation_data = load_batch("cifar-10-batches-py/data_batch_2")
@@ -12,8 +14,10 @@ def load_data():
             'test_data': test_data}
 
 def preprocessing(data):
-    mean_x = np.mean(data['train_data']['data'],axis=0)
-    std_x = np.std(data['train_data']['data'],axis=0)
+    mean_x = np.mean(data['train_data']['data'],axis=1)
+    mean_x = mean_x[:, np.newaxis]
+    std_x = np.std(data['train_data']['data'],axis=1)
+    std_x = std_x[:,np.newaxis]
     data['train_data']['data'] = (data['train_data']['data'] - mean_x)/std_x
     data['test_data']['data'] = (data['test_data']['data'] - mean_x) / std_x
     data['validation_data']['data'] = (data['validation_data']['data'] - mean_x) / std_x
@@ -24,12 +28,14 @@ def check_matrices(a_anal,a_num):
     matrix = np.abs(a_anal - a_num)
     return np.isin(matrix <= 1e-6, True).all()
 
+
+
 def main():
     data = load_data()
     data = preprocessing(data)
     classifier = Classifier(dim_images=len(data['train_data']['data']),
                             num_labels=len(data['train_data']['one_hot']))
-
+    """
     prediction = classifier.predict(data['train_data']['data'])
     cost = classifier.compute_cost(data['train_data']['data'],data['train_data']['one_hot'],prediction)
     print("Computed cost:", cost)
@@ -39,6 +45,11 @@ def main():
     gradient_num = classifier.ComputeGradsNum(data['train_data']['data'][:, :20],data['train_data']['one_hot'][:, :20],prediction,classifier.W,classifier.b,0,1e-6)
     print("gradient_w well calculated:",check_matrices(j_wrt_w,gradient_num[0]))
     print("gradient_b well calculated:", check_matrices(j_wrt_b, gradient_num[1]))
+    """
+    classifier.mini_batch(data['train_data']['data'],data['train_data']['one_hot'],n_batch=100,eta=0.001,n_epochs=40,lamda=0)
+    prediction_test = classifier.predict(data['test_data']['data'])
+    acc_test = classifier.compute_accuracy(data['test_data'],prediction_test)
+    print("Accuracy in test set:", acc_test)
 
 if __name__ == "__main__":
     main()

classifier.py
@@ -10,7 +10,7 @@ class Classifier:
     def predict(self, X):
         return softmax(np.dot(self.W,X)+self.b)
 
-    def compute_cost(self,X,labels_onehot,prediction,lambda_reg=1.0):
+    def compute_cost(self,X,labels_onehot,prediction,lambda_reg=0.0):
         """Equation number (5) in the paper"""
         num_datapoints = X.shape[1]
         entr = self._cross_entropy(labels_onehot, prediction)
@@ -35,6 +35,27 @@ class Classifier:
         j_wrt_b = 1/n*np.dot(g_batch,np.ones((n,1)))
         return j_wtr_w,j_wrt_b
 
+
+
+    def mini_batch(self,X,Y,n_batch,eta,n_epochs,lamda):
+        n = X.shape[1]
+        for _ in range(n_epochs):
+            for j in range(int(n/n_batch)):
+                j = j + 1
+                j_start = (j - 1) * n_batch + 1
+                j_end = j*n_batch
+                X_batch = X[:, j_start:j_end]
+                Y_batch = Y[:,j_start:j_end]
+                prediction = self.predict(X_batch)
+                j_wtr_w,j_wrt_b = self.compute_gradients(X_batch,Y_batch,prediction,lamda)
+                self.W = self.W - eta*j_wtr_w
+                self.b = self.b - eta*j_wrt_b
+            prediction_all = self.predict(X)
+            cost = self.compute_cost(X, Y, prediction_all, lamda)
+            print(cost)
+
+
+    #Analytical functions
     def ComputeCost(self, X, Y, W, b, lamda):
         """Equation number (5) in the paper"""
         num_datapoints = X.shape[1]