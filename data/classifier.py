12fd11be0cb17563d29e61b08c7fdb4a18d9f887
annasanchez27 <annasanchezespunyes27@gmail.com>
2021-04-06 22:39:08 +0200

    svm implemented

--- data/classifier.py
+++ data/classifier.py
@@ -9,75 +9,101 @@ class Classifier:
         self.W = np.random.normal(0, 0.01, size=(num_labels, dim_images))
         self.b = np.random.normal(0, 0.01, size=(num_labels, 1))
 
-    def predict(self, X):
-        return softmax(np.dot(self.W,X)+self.b)
+    def predict(self, X, loss_function):
+        if loss_function == 'cross-entropy':
+            return softmax(np.dot(self.W,X)+self.b)
+        if loss_function == 'svm':
+            return np.dot(self.W, X) + self.b
 
-    def compute_cost(self,X,labels_onehot,prediction,lambda_reg=0.0):
+
+    def compute_cost(self,X,labels_onehot,prediction,loss_function,lambda_reg=0.0):
         """Equation number (5) in the paper"""
         num_datapoints = X.shape[1]
-        entr = self._cross_entropy(labels_onehot, prediction)
+        entr = self._loss(labels_onehot, prediction,loss_function)
         return 1/num_datapoints*np.sum(entr)+ lambda_reg*np.sum(np.square(self.W))
 
 
-    def _cross_entropy(self,label_onehotencoded,probabilities):
+    def _loss(self,label_onehotencoded,probabilities,loss_function):
         """Equation number (6) in the assigment"""
-        r = -np.log(np.sum(label_onehotencoded * probabilities,axis=0))
-        return r
+        if loss_function == 'cross-entropy':
+            return -np.log(np.sum(label_onehotencoded * probabilities,axis=0))
+        if loss_function == 'svm':
+            s_y = np.sum(label_onehotencoded * probabilities, axis=0)
+            s = np.maximum(np.zeros(probabilities.shape),probabilities-s_y+1)
+            s[label_onehotencoded.astype(bool)] = 0
+            return np.sum(s,axis=0)
+
+
 
     def compute_accuracy(self,data,prediction):
         """Equation number (4) in the assigment"""
         pred = np.argmax(prediction,axis=0)
         return np.sum(pred == data['labels'])/len(prediction[0])*100
 
-    def compute_gradients(self,X,labels_onehot,predictions,lambda_reg):
+    def compute_gradients(self,X,labels_onehot,predictions,loss_function,lambda_reg):
         """Equation (10) and (11) in the assigment. Look last slides Lecture 3"""
         n = X.shape[1]
-        g_batch = -(labels_onehot-predictions)
-        j_wtr_w = 1/n*np.dot(g_batch,X.T) + 2*lambda_reg*self.W
-        j_wrt_b = 1/n*np.dot(g_batch,np.ones((n,1)))
-        return j_wtr_w,j_wrt_b
+        if loss_function == 'cross-entropy':
+            g_batch = -(labels_onehot-predictions)
+            j_wtr_w = 1/n*np.dot(g_batch,X.T) + 2*lambda_reg*self.W
+            j_wrt_b = 1/n*np.dot(g_batch,np.ones((n,1)))
+            return j_wtr_w,j_wrt_b
+
+        if loss_function == 'svm':
+            '''
+            Loss gradient for SGD with batch size 1 only
+            link to where I took the formula from: https://cs231n.github.io/optimization-1/
+            '''
+            s_y = np.sum(labels_onehot * predictions, axis=0)
+            s = np.maximum(np.zeros(predictions.shape), predictions - s_y + 1)
+            s[labels_onehot.astype(bool)] = 0
+            vector = s
+            vector[s > 0] = 1
+            # count the number of classes that didn’t meet the desired margin
+            num_misclass = np.sum(vector, axis=0)
+            # in the right class we need to put the previous count
+            vector[np.argmax(labels_onehot, axis=0) , np.arange(n)] = -num_misclass
+            j_wtr_w = np.dot(vector, X.T) / n + lambda_reg * self.W
+            j_wrt_b = 1/n*np.dot(vector,np.ones((n,1)))
+            return j_wtr_w,j_wrt_b
+
+    def fit(self,X_train,Y_train,X_val,Y_val,loss_function,n_batch,eta,n_epochs,lamda):
+            n = X_train.shape[1]
+            error_train =[]
+            error_val = []
+            for i in range(n_epochs):
+                indices = np.arange(Y_train.shape[1])
+                np.random.shuffle(indices)
+                X_train = X_train[:,indices]
+                Y_train = Y_train[:,indices]
+                for j in range(int(n/n_batch)):
+                    j = j + 1
+                    j_start = (j - 1) * n_batch + 1
+                    j_end = j*n_batch
+                    X_batch = X_train[:, j_start:j_end]
+                    Y_batch = Y_train[:,j_start:j_end]
+                    prediction = self.predict(X_batch,loss_function)
+                    j_wtr_w,j_wrt_b = self.compute_gradients(X_batch,Y_batch,prediction,loss_function,lamda)
+                    self.W = self.W - eta*j_wtr_w
+                    self.b = self.b - eta*j_wrt_b
+                eta = 0.9*eta
+                prediction_train = self.predict(X_train,loss_function)
+                cost_train = self.compute_cost(X_train, Y_train, prediction_train, loss_function,lamda)
+                prediction_val = self.predict(X_val,loss_function)
+                cost_val = self.compute_cost(X_val, Y_val, prediction_val, loss_function,lamda)
+                print("Epoch #" + str(i) + " Loss:" + str(cost_val))
+                error_train.append(cost_train)
+                error_val.append(cost_val)
+
+            return {'loss_train':error_train,
+                    'loss_val':error_val}
 
 
-
-    def mini_batch(self,X_train,Y_train,X_val,Y_val,n_batch,eta,n_epochs,lamda):
-        n = X_train.shape[1]
-        error_train =[]
-        error_val = []
-        for i in range(n_epochs):
-            indices = np.arange(Y_train.shape[1])
-            np.random.shuffle(indices)
-            X_train = X_train[:,indices]
-            Y_train = Y_train[:,indices]
-            for j in range(int(n/n_batch)):
-                j = j + 1
-                j_start = (j - 1) * n_batch + 1
-                j_end = j*n_batch
-                X_batch = X_train[:, j_start:j_end]
-                Y_batch = Y_train[:,j_start:j_end]
-                prediction = self.predict(X_batch)
-                j_wtr_w,j_wrt_b = self.compute_gradients(X_batch,Y_batch,prediction,lamda)
-                self.W = self.W - eta*j_wtr_w
-                self.b = self.b - eta*j_wrt_b
-
-            eta = 0.9*eta
-            prediction_train = self.predict(X_train)
-            cost_train = self.compute_cost(X_train, Y_train, prediction_train, lamda)
-            prediction_val = self.predict(X_val)
-            cost_val = self.compute_cost(X_val, Y_val, prediction_val, lamda)
-            print("Epoch #" + str(i) + " Loss:" + str(cost_val))
-            error_train.append(cost_train)
-            error_val.append(cost_val)
-
-        return {'loss_train':error_train,
-                'loss_val':error_val}
-
-
-    #Analytical functions
     def ComputeCost(self, X, Y, W, b, lamda):
         """Equation number (5) in the paper"""
         num_datapoints = X.shape[1]
         prediction = self.Predict(X, W, b)
-        entr = self._cross_entropy(Y, prediction)
+        entr = self._loss(Y, prediction,'cross-entropy')
         return 1 / num_datapoints * np.sum(entr) + lamda * np.sum(np.square(W))
 
     def Predict(self, X, W, b):
@@ -91,12 +117,12 @@ class Classifier:
         grad_W = np.zeros(W.shape);
         grad_b = np.zeros((no, 1));
 
-        c = self.ComputeCost(X, Y, W, b, lamda);
+        c = self.ComputeCost(X, Y, W, b,lamda);
 
         for i in range(len(b)):
             b_try = np.array(b)
             b_try[i] += h
-            c2 = self.ComputeCost(X, Y, W, b_try, lamda)
+            c2 = self.ComputeCost(X, Y, W, b_try,lamda)
             grad_b[i] = (c2 - c) / h
 
         for i in range(W.shape[0]):
--- src/Assigment1.py
+++ src/Assigment1.py
@@ -8,36 +8,40 @@ from utils import montage,error_plot
 
 def check_matrices(a_anal,a_num):
     matrix = np.abs(a_anal - a_num)
-    return np.isin(matrix <= 1e-6, True).all()
+    return np.isin(matrix <= 1e-7, True).all()
 
 
+def gradient_exercise(classifier,data):
+    prediction = classifier.predict(data['train_data']['data'],'cross-entropy')
+    cost = classifier.compute_cost(data['train_data']['data'], data['train_data']['one_hot'], prediction,'cross-entropy')
+    print("Computed cost:", cost)
+    accuracy = classifier.compute_accuracy(data['train_data'], prediction)
+    print("Computed accuracy:", accuracy)
+    j_wrt_w, j_wrt_b = classifier.compute_gradients(data['train_data']['data'][:, :20],
+                                                    data['train_data']['one_hot'][:, :20], prediction[:, :20],'cross-entropy' ,0)
+    gradient_num = classifier.ComputeGradsNum(data['train_data']['data'][:, :20], data['train_data']['one_hot'][:, :20],
+                                              prediction, classifier.W, classifier.b, 0, 1e-6)
+    print("gradient_w well calculated:", check_matrices(j_wrt_w, gradient_num[0]))
+    print("gradient_b well calculated:", check_matrices(j_wrt_b, gradient_num[1]))
 
 def main():
-    data = load_data()
+    data = load_data(exc2=True)
     data = preprocessing(data)
     print("Preprocessing done!")
     classifier = Classifier(dim_images=len(data['train_data']['data']),
                             num_labels=len(data['train_data']['one_hot']))
-    """
-    prediction = classifier.predict(data['train_data']['data'])
-    cost = classifier.compute_cost(data['train_data']['data'],data['train_data']['one_hot'],prediction)
-    print("Computed cost:", cost)
-    accuracy = classifier.compute_accuracy(data['train_data'],prediction)
-    print("Computed accuracy:", accuracy)
-    j_wrt_w,j_wrt_b = classifier.compute_gradients(data['train_data']['data'][:, :20],data['train_data']['one_hot'][:, :20],prediction[:, :20],0)
-    gradient_num = classifier.ComputeGradsNum(data['train_data']['data'][:, :20],data['train_data']['one_hot'][:, :20],prediction,classifier.W,classifier.b,0,1e-6)
-    print("gradient_w well calculated:",check_matrices(j_wrt_w,gradient_num[0]))
-    print("gradient_b well calculated:", check_matrices(j_wrt_b, gradient_num[1]))
-    """
-
-    loss = classifier.mini_batch(data['train_data']['data'],data['train_data']['one_hot'],
-                                        data['validation_data']['data'],data['validation_data']['one_hot'],
-                                        n_batch=80,eta=0.01,n_epochs=50,lamda=0)
-    error_plot(loss['loss_train'],loss['loss_val'])
-    prediction_test = classifier.predict(data['test_data']['data'])
+    gradient_exercise(classifier,data)
+    loss = classifier.fit(data['train_data']['data'], data['train_data']['one_hot'],
+                          data['validation_data']['data'], data['validation_data']['one_hot'],
+                          loss_function='cross-entropy',
+                          n_batch=80, eta=0.01, n_epochs=10, lamda=0)
+    error_plot(loss['loss_train'], loss['loss_val'])
+    prediction_test = classifier.predict(data['test_data']['data'], loss_function='cross-entropy')
     montage(classifier.W)
-    acc_test = classifier.compute_accuracy(data['test_data'],prediction_test)
+    acc_test = classifier.compute_accuracy(data['test_data'], prediction_test)
     print("Accuracy in test set:", acc_test)
+
+
 
 if __name__ == "__main__":
     main()
